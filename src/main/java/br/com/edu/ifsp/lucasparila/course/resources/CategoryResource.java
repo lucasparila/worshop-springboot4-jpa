e90b527fdd16df2f461590d321e5dbb05d3dacf9
lucasparila <lucas.eparila@gmail.com>
2026-03-28 15:24:08 -0300

    Category

---
+++ src/main/java/br/com/edu/ifsp/lucasparila/course/resources/CategoryResource.java
@@ -0,0 +1,34 @@
+package br.com.edu.ifsp.lucasparila.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.edu.ifsp.lucasparila.course.entities.Category;
+import br.com.edu.ifsp.lucasparila.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+	
+	@Autowired
+	private CategoryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		
+		List<Category> categories = service.findAll();
+		return ResponseEntity.ok().body(categories);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Category> findById(@PathVariable Long id){
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
